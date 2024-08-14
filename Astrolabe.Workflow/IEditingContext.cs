4922c88a80cfd6499ea7d9fece50ac63cb784137
Jolse Maginnis <doolse@gmail.com>
2024-08-14 12:18:33 +1000

    Allow passing in new flag

--- Astrolabe.Workflow/IEditingContext.cs
+++ Astrolabe.Workflow/IEditingContext.cs
@@ -7,20 +7,24 @@ public interface IEditingContext<out T>
 
     bool Changed { get; }
 
-    IEditingContext<T2>? As<T2>() where T2 : class =>
-        Edited is T2 e ? new SimpleEditingContext<T2>(Original as T2, e) : null;
+    bool New { get; }
+    IEditingContext<T2>? As<T2>()
+        where T2 : class =>
+        Edited is T2 e ? new SimpleEditingContext<T2>(Original as T2, e, New) : null;
 }
 
-public record SimpleEditingContext<T>(T? Original, T Edited) : IEditingContext<T>
+public record SimpleEditingContext<T>(T? Original, T Edited, bool New) : IEditingContext<T>
 {
     public bool Changed => !Equals(Original, Edited);
 }
 
-public class MappedEditingContext<T, T2>(IEditingContext<T> originalContext, Func<T, T2> mapper) : IEditingContext<T2>
+public class MappedEditingContext<T, T2>(IEditingContext<T> originalContext, Func<T, T2> mapper)
+    : IEditingContext<T2>
 {
-    public T2? Original { get; } = originalContext.Original is { } v ? mapper(v!) : default;
+    public T2? Original { get; } = originalContext.Original is { } v ? mapper(v) : default;
     public T2 Edited { get; } = mapper(originalContext.Edited);
     public bool Changed => !Equals(Original, Edited);
+    public bool New => originalContext.New;
 }
 
 public static class WorkflowEditContextExtensions
@@ -28,19 +32,28 @@ public static class WorkflowEditContextExtensions
     public static IEditingContext<T2> Map<T, T2>(this IEditingContext<T> original, Func<T, T2> m) =>
         new MappedEditingContext<T, T2>(original, m);
 
-    public static IEditingContext<T> MakeContext<T>(T? original, T edited)
+    public static IEditingContext<T> MakeContext<T>(T? original, T edited, bool isNew = false)
     {
-        return new SimpleEditingContext<T>(original, edited);
+        return new SimpleEditingContext<T>(original, edited, isNew);
     }
 
-    public static bool ChangedTo<T, TValue>(this IEditingContext<T> context, Func<T, TValue> getter, TValue compare)
+    public static bool ChangedTo<T, TValue>(
+        this IEditingContext<T> context,
+        Func<T, TValue> getter,
+        TValue compare
+    )
     {
-        if (!Equals(getter(context.Edited), compare)) return false;
+        if (!Equals(getter(context.Edited), compare))
+            return false;
         var orig = context.Original;
         return orig == null || !Equals(getter(orig), compare);
     }
 
-    public static bool ChangedFrom<T, TValue>(this IEditingContext<T> context, Func<T, TValue> getter, TValue compare)
+    public static bool ChangedFrom<T, TValue>(
+        this IEditingContext<T> context,
+        Func<T, TValue> getter,
+        TValue compare
+    )
     {
         var orig = context.Original;
         if (orig == null)
