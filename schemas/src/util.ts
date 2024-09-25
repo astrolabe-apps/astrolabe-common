bad8af48589637baadb074cc95e3aef202696ea9
Jolse Maginnis <doolse@gmail.com>
2024-09-25 11:11:45 +1000

    No append element

--- schemas/src/util.ts
+++ schemas/src/util.ts
@@ -6,7 +6,6 @@ import {
   DataRenderType,
   DisplayOnlyRenderOptions,
   FieldOption,
-  FieldType,
   findField,
   GroupedControlsDefinition,
   isCompoundField,
@@ -662,13 +661,6 @@ export function toDepString(x: any): string {
   if (x === undefined) return "_";
   if (x === null) return "~";
   return x.toString();
-}
-
-export function appendElementIndex(
-  dataContext: ControlDataContext,
-  elementIndex: number,
-): ControlDataContext {
-  return { ...dataContext, path: [...dataContext.path, elementIndex] };
 }
 
 export function applyLengthRestrictions<Min, Max>(
