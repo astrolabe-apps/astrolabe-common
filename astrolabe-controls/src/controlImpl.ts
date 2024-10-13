7c9822ef2184872e02764361e7de66e209f6e003
Jolse Maginnis <doolse@gmail.com>
2024-10-13 12:07:13 +1100

    chidren

--- astrolabe-controls/src/controlImpl.ts
+++ astrolabe-controls/src/controlImpl.ts
@@ -10,6 +10,7 @@ export enum ControlFlags {
   StructureChanged = 32,
   NeedsValidate = 64,
   ValueMutating = 128,
+  InitialValueMutating = 256,
 }
 
 export enum ControlChange {
@@ -41,14 +42,14 @@ export enum StateFlags {
 }
 
 interface ParentListeners {
-  childValueChange(v: unknown, from: ControlImpl<unknown> | undefined): void;
+  childValueChange(v: unknown, ignore: ControlImpl<unknown> | undefined): void;
   childInitialValueChange(
     v: unknown,
-    from: ControlImpl<unknown> | undefined,
+    ignore: ControlImpl<unknown> | undefined,
   ): void;
   childFlagChange(
     flags: ControlFlags,
-    from: ControlImpl<unknown> | undefined,
+    ignore: ControlImpl<unknown> | undefined,
   ): void;
 }
 
@@ -59,87 +60,55 @@ interface ParentListener {
   childFlagChange(prop: string | number, flags: ControlFlags): void;
 }
 
-interface ControlStateImpl<V> {
-  getValue(): V;
-  getInitialValue(): V;
+interface ChildState<V> extends ParentListener {
   setValue(v: V, from?: ControlImpl<unknown>): boolean;
-  getFields(): V extends string | number | Array<any> | undefined | null
-    ? undefined
-    : V extends { [a: string]: any }
-      ? { [K in keyof V]-?: Control<V[K]> }
-      : V;
 }
 
 export class ControlImpl<V> {
-  state: ControlStateImpl<V>;
-
   constructor(
-    initState: (c: ControlImpl<V>) => ControlStateImpl<V>,
     public _value: V,
     public _initialValue: V,
     public _flags: ControlFlags,
-    public _parents: ParentListeners | undefined,
+    public _parents?: ParentListeners,
     public _errors?: { [k: string]: string },
-  ) {
-    this.state = initState(this);
-  }
+    public _children?: ChildState<V>,
+  ) {}
 
   get value(): V {
-    return this.state.getValue();
+    this._flags &= ~ControlFlags.ValueMutating;
+    return this._value;
   }
 
-  get initialValue(): V {
-    return this.state.getInitialValue();
-  }
-
-  set value(v: V) {
-    this.state.setValue(v);
-  }
-
-  get fields(): V extends string | number | Array<any> | undefined | null
-    ? undefined
-    : V extends { [a: string]: any }
-      ? { [K in keyof V]-?: Control<V[K]> }
-      : V {
-    return this.state.getFields();
-  }
-}
-
-class ValueState<V> implements ControlStateImpl<V> {
-  constructor(public control: ControlImpl<V>) {}
-
-  getFields(): V extends string | number | any[] | null | undefined
-    ? undefined
-    : V extends { [a: string]: any }
-      ? { [K in keyof V]-?: Control<V[K]> }
-      : V {
-    const objectControl = new ObjectControl(this.control);
-    this.control.state = objectControl;
-    return objectControl.getFields();
-  }
-
-  getValue(): V {
-    return this.control._value;
-  }
-  getInitialValue(): V {
-    return this.control._initialValue;
-  }
-  setValue(v: V, from?: ControlImpl<unknown>): boolean {
-    const c = this.control;
-    const changed = c._value !== v;
+  setValue(v: V, dontUpdate?: ControlImpl<unknown>): boolean {
+    if (this._children) return this._children.setValue(v, dontUpdate);
+    const changed = this._value !== v;
     if (changed) {
-      c._value = v;
-      this.control._parents?.childValueChange(v, from);
+      this._value = v;
+      this._parents?.childValueChange(v, dontUpdate);
     }
     return changed;
   }
+
+  get initialValue(): V {
+    this._flags &= ~ControlFlags.InitialValueMutating;
+    return this._initialValue;
+  }
+
+  set value(v: V) {
+    this.setValue(v);
+  }
+
+  get fields(): { [K in keyof V]-?: Control<V[K]> } {
+    if (!this._children) {
+      this._children = new ObjectControl<V>(this);
+    }
+    return (this._children as ObjectControl<any>).getFields();
+  }
 }
 
-class ObjectControl<V> extends ValueState<V> implements ParentListener {
+class ObjectControl<V> implements ChildState<V> {
   private _fields: Record<string, ControlImpl<unknown>> = {};
-  constructor(control: ControlImpl<V>) {
-    super(control);
-  }
+  constructor(public control: ControlImpl<V>) {}
   getValue(): V {
     const c = this.control;
     c._flags &= ~ControlFlags.ValueMutating;
@@ -151,7 +120,7 @@ class ObjectControl<V> extends ValueState<V> implements ParentListener {
     const c = this.control;
     Object.entries(this._fields).forEach(([k, fv]) => {
       const cv = (v as any)[k];
-      changed ||= fv.state.setValue(cv, c);
+      changed ||= fv.setValue(cv, c);
     });
     if (changed) {
       c._value = v;
@@ -175,7 +144,6 @@ class ObjectControl<V> extends ValueState<V> implements ParentListener {
     const v = _value != null ? (_value as any)[p] : undefined;
     const iv = _initialValue != null ? (_initialValue as any)[p] : undefined;
     const c = new ControlImpl(
-      (c) => new ValueState(c),
       v,
       iv,
       (this.control._flags & ControlFlags.Disabled) |
@@ -216,26 +184,23 @@ class SingleParent implements ParentListeners {
     private parent: ParentListener,
     private child: string | number,
   ) {}
-  childValueChange(v: unknown, from?: ControlImpl<unknown>): void {
-    if (this.parent.control === from) return;
+  childValueChange(v: unknown, ignore: ControlImpl<unknown> | undefined): void {
+    if (this.parent.control === ignore) return;
     this.parent.childValueChange(this.child, v);
   }
-  childInitialValueChange(v: unknown, from?: ControlImpl<unknown>): void {
-    if (this.parent.control === from) return;
+  childInitialValueChange(v: unknown, ignore: ControlImpl<unknown>): void {
+    if (this.parent.control === ignore) return;
     this.parent.childInitialValueChange(this.child, v);
   }
-  childFlagChange(flags: ControlFlags, from?: ControlImpl<unknown>): void {
-    if (this.parent.control === from) return;
+  childFlagChange(
+    flags: ControlFlags,
+    ignore: ControlImpl<unknown> | undefined,
+  ): void {
+    if (this.parent.control === ignore) return;
     this.parent.childFlagChange(this.child, flags);
   }
 }
 
 export function newControl<V>(v: V) {
-  return new ControlImpl(
-    (c) => new ValueState(c),
-    v,
-    v,
-    ControlFlags.None,
-    undefined,
-  );
+  return new ControlImpl(v, v, ControlFlags.None, undefined);
 }
