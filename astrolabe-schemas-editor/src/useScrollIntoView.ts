58850873262281e057046f1f5076333056916590
Jolse Maginnis <doolse@gmail.com>
2024-11-07 12:01:47 +1100

    Fix scrolling selected control

--- astrolabe-schemas-editor/src/useScrollIntoView.ts
+++ astrolabe-schemas-editor/src/useScrollIntoView.ts
@@ -7,18 +7,21 @@ export function useScrollIntoView<E extends HTMLElement = HTMLDivElement>(
 ) {
   const inViewControl = useControl(true);
   const itemRef = useRef<HTMLElement | null>();
+  const haveScrolled = useRef(false);
   const setElement = useInViewEffect(
     ([entry], observer) => (inViewControl.value = entry.isIntersecting),
   );
   useControlEffect(
     () => [inViewControl.value, shouldBeInView],
     ([iv, sbiv]) => {
-      if (!iv && sbiv && itemRef.current) {
+      if (!iv && sbiv && itemRef.current && !haveScrolled.current) {
+        haveScrolled.current = true;
         itemRef.current.scrollIntoView({
           behavior: "smooth",
           block: "start",
         });
       }
+      if (!sbiv) haveScrolled.current = false;
     },
   );
   return (e: HTMLElement | null) => {
