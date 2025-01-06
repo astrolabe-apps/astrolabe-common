7fa8f3f8908b809384f6e7467225ef2b7b3eee9e
Jolse Maginnis <doolse@gmail.com>
2025-01-06 13:45:44 +1100

    Get edit expression

--- Astrolabe.ColumnEditor/ColumnEditor.cs
+++ Astrolabe.ColumnEditor/ColumnEditor.cs
@@ -1,10 +1,11 @@
+using System.Linq.Expressions;
+
 namespace Astrolabe.ColumnEditor;
 
 public interface ColumnEditor<TEdit, TDb> : ColumnDbExtractor<TDb>
 {
     public Dictionary<string, object> Attributes { get; }
     public Func<TEdit, ColumnContext<TDb>, Task<ColumnContext<TDb>>> Edit { get; }
-    
-    
 
+    Expression? EditGetterExpression { get; }
 }
