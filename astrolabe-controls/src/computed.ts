35a62347df2558d4f3bd5c3909eda540bc80a331
Jolse Maginnis <doolse@gmail.com>
2025-01-06 21:51:22 +1100

    Force value change for initial computation

--- astrolabe-controls/src/computed.ts
+++ astrolabe-controls/src/computed.ts
@@ -1,20 +1,28 @@
-import { Control } from "./types";
+import { Control, ControlChange } from "./types";
 import { getInternalMeta, InternalControl } from "./internal";
 import { createEffect } from "./effect";
 import { addCleanup } from "./controlImpl";
+import { runTransaction } from "./transactions";
 
-
-export function updateComputedValue<V>(control: Control<V>, compute: () => V): void {
-	const c = control as InternalControl<V>;
-	const meta = getInternalMeta(c) ?? {};
-	if (meta.compute?.calculate === compute) return;
-	if (!meta.compute)
-	{
-		const effect = createEffect(compute, (v) => c.setValueImpl(v));
-		meta.compute = effect;
-		addCleanup(c, () => effect.cleanup());
-	} else {
-		meta.compute.calculate = compute;
-		meta.compute.runEffect();
-	}
+export function updateComputedValue<V>(
+  control: Control<V>,
+  compute: () => V,
+  forceValueChange?: boolean,
+): void {
+  const c = control as InternalControl<V>;
+  const meta = getInternalMeta(c) ?? {};
+  if (meta.compute?.calculate === compute) return;
+  if (!meta.compute) {
+    const effect = createEffect(compute, (v) => c.setValueImpl(v));
+    meta.compute = effect;
+    addCleanup(c, () => effect.cleanup());
+    if (forceValueChange) {
+      runTransaction(c, () => {
+        c._subscriptions?.applyChange(ControlChange.Value);
+      });
+    }
+  } else {
+    meta.compute.calculate = compute;
+    meta.compute.runEffect();
+  }
 }
