2ff2f0fbc63bc88fc1f2b4d91cdb26d7f1cd218b
Jolse Maginnis <doolse@gmail.com>
2025-01-06 16:58:47 +1100

    Fix transactions

--- astrolabe-controls/src/transactions.ts
+++ astrolabe-controls/src/transactions.ts
@@ -5,50 +5,18 @@ let transactionCount = 0;
 let runListenerList: InternalControl<any>[] = [];
 let afterChangesCallbacks: (() => void)[] = [];
 
-export function runPendingChanges() {}
-export function unsafeFreezeCountEdit(dir: number) {
-  if (dir == 1) transactionCount++;
-  else commit(undefined);
-}
-
-export function addAfterChangesCallback(cb: () => void) {
-  afterChangesCallbacks.push(cb);
-}
-export function groupedChanges<A>(run: () => A): A {
-  transactionCount++;
-  try {
-    return run();
-  } finally {
-    commit(undefined);
-  }
-}
-
-export function runTransaction(control: Control<any>, run: () => void): void {
-  transactionCount++;
-  try {
-    run();
-  } finally {
-    commit(control as InternalControl<unknown>);
-  }
-}
-
-export function commit(control?: InternalControl<unknown>) {
-  const sub = control?._subscriptions;
-  if (transactionCount > 1) {
-    if (sub && !sub.onListenerList) {
-      sub.onListenerList = true;
-      runListenerList.push(control);
-    }
-    transactionCount--;
-    return;
-  } else {
-    if (!runListenerList.length && sub) {
-      control!.runListeners();
-    } else {
-      if (sub) runListenerList.push(control);
-    }
-    while (transactionCount === 1) {
-      while (runListenerList.length > 0) {
+export function runPendingChanges() {
+  while (
+    transactionCount == 0 &&
+    (afterChangesCallbacks.length || runListenerList.length)
+  ) {
+    try {
+      unsafeFreezeCountEdit(1);
+      if (runListenerList.length == 0) {
+        const cbToRun = afterChangesCallbacks;
+        afterChangesCallbacks = [];
+        cbToRun.forEach((cb) => cb());
+      } else {
         const listenersToRun = runListenerList;
         runListenerList = [];
         listenersToRun.forEach(
@@ -56,10 +24,51 @@ export function commit(control?: InternalControl<unknown>) {
         );
         listenersToRun.forEach((c) => c.runListeners());
       }
-      const cbToRun = afterChangesCallbacks;
-      afterChangesCallbacks = [];
-      cbToRun.forEach((cb) => cb());
-      transactionCount--;
+    } finally {
+      unsafeFreezeCountEdit(-1);
     }
+  }
+}
+export function unsafeFreezeCountEdit(dir: number) {
+  transactionCount += dir;
+}
+
+export function addAfterChangesCallback(cb: () => void) {
+  afterChangesCallbacks.push(cb);
+}
+export function groupedChanges<A>(run: () => A): A {
+  unsafeFreezeCountEdit(1);
+  try {
+    return run();
+  } finally {
+    unsafeFreezeCountEdit(-1);
+    runPendingChanges();
+  }
+}
+
+export function runTransaction(control: Control<any>, run: () => void): void {
+  unsafeFreezeCountEdit(1);
+  try {
+    run();
+  } catch (e) {
+    console.error("Error in control", e);
+  }
+
+  const c = control as InternalControl<any>;
+  const sub = c._subscriptions;
+  if (transactionCount > 1) {
+    if (sub && !sub.onListenerList) {
+      sub.onListenerList = true;
+      runListenerList.push(c);
+    }
+    unsafeFreezeCountEdit(-1);
+  } else {
+    if (!runListenerList.length && sub) {
+      c.runListeners();
+    } else {
+      if (sub) runListenerList.push(c);
+    }
+    unsafeFreezeCountEdit(-1);
+    runPendingChanges();
   }
 }
