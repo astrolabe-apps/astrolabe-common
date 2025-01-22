58d6065028548498dc769dbadd59dd5e6494cc4d
Jolse Maginnis <doolse@gmail.com>
2025-01-22 14:18:06 +1100

    Support notValue in checkbox

--- core/src/Fcheckbox.tsx
+++ core/src/Fcheckbox.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { formControlProps, useControlEffect } from "./hooks";
-import {Control} from "@astroapps/controls";
+import { Control } from "@astroapps/controls";
 
 export type FcheckboxProps = React.InputHTMLAttributes<HTMLInputElement> & {
   control: Control<boolean | undefined | null>;
   type?: "checkbox" | "radio";
+  notValue?: boolean;
 };
 
 export function Fcheckbox({
   control,
   type = "checkbox",
+  notValue = false,
   ...others
 }: FcheckboxProps) {
   // Update the HTML5 custom validity whenever the error message is changed/cleared
@@ -22,12 +24,12 @@ export function Fcheckbox({
   return (
     <input
       {...theseProps}
-      checked={!!value}
+      checked={!!value !== notValue}
       ref={(r) => {
         control.element = r;
         if (r) r.setCustomValidity(control.current.error ?? "");
       }}
-      onChange={(e) => (control.value = e.target.checked)}
+      onChange={(e) => (control.value = e.target.checked !== notValue)}
       type={type}
       {...others}
     />
