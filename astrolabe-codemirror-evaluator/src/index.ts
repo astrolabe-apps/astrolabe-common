f2f11812f28ba77cf3c24bdacaa5660be18f00f3
Jolse Maginnis <doolse@gmail.com>
2025-03-04 17:34:27 +1100

    Better highlights

--- astrolabe-codemirror-evaluator/src/index.ts
+++ astrolabe-codemirror-evaluator/src/index.ts
@@ -1,30 +1,37 @@
-import {parser} from "@astroapps/evaluator"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import { parser } from "@astroapps/evaluator";
+import {
+  delimitedIndent,
+  foldInside,
+  foldNodeProp,
+  indentNodeProp,
+  LanguageSupport,
+  LRLanguage,
+} from "@codemirror/language";
+import { styleTags, tags as t } from "@lezer/highlight";
 
 export const EvalLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Application: delimitedIndent({ closing: ")", align: false }),
       }),
       foldNodeProp.add({
-        Application: foldInside
+        Application: foldInside,
       }),
       styleTags({
-        Reference: t.variableName,
-        BooleanLiteral: t.bool,
+        Reference: t.definition(t.variableName),
+        ConstantLiteral: t.bool,
         String: t.string,
         Number: t.number,
-        "( )": t.paren
-      })
-    ]
+        "( )": t.paren,
+      }),
+    ],
   }),
   languageData: {
-    commentTokens: {line: ";"}
-  }
-})
+    commentTokens: { line: ";" },
+  },
+});
 
 export function Evaluator() {
-  return new LanguageSupport(EvalLanguage)
+  return new LanguageSupport(EvalLanguage);
 }
