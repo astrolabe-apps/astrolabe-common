325fe2fd6751b5aa8efc23399fc615d73328035f
Connor <yucong.zhao@itechnologies.com.au>
2025-05-22 16:44:26 +1000

    Fix

--- schemas-rn/src/components/RNSelectRenderer.tsx
+++ schemas-rn/src/components/RNSelectRenderer.tsx
@@ -162,7 +162,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex flex-row h-10 native:h-12 items-center text-sm justify-between border border-[#E7E7E8] bg-background px-3 py-2 web:ring-offset-background text-muted-foreground web:focus:outline-none web:focus:ring-2 web:focus:ring-ring web:focus:ring-offset-2 [&>span]:line-clamp-1",
+      "flex flex-row h-10 native:h-12 items-center text-sm justify-between border border-[#E7E7E8] bg-background px-3 py-2 web:ring-offset-background text-muted-foreground web:focus:outline-none web:focus:ring-2 web:focus:ring-ring web:focus:ring-offset-2 [&>span]:line-clamp-1 min-w-[128px]",
       props.disabled && "web:cursor-not-allowed opacity-50",
       className,
     )}
