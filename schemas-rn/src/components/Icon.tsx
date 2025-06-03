37faaadf8bea7399f4b5e5c0130311c05d799ede
Connor <yucong.zhao@itechnologies.com.au>
2025-06-03 10:43:27 +1000

    Update Icon renderer

--- schemas-rn/src/components/Icon.tsx
+++ schemas-rn/src/components/Icon.tsx
@@ -2,6 +2,7 @@
 import { FontAwesome } from "@expo/vector-icons";
 import { IconLibrary } from "@react-typed-forms/schemas";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 export type RNIconProps = {
   name: string;
@@ -23,6 +24,13 @@ cssInterop(MaterialIcons, {
   },
 });
 
+cssInterop(FontAwesomeIcon, {
+  className: {
+    target: "style",
+    nativeStyleToProp: {},
+  },
+});
+
 export function Icon({
   name,
   className = "!text-[16px] text-black-500",
@@ -31,7 +39,18 @@ export function Icon({
   switch (iconLibrary) {
     case IconLibrary.Material:
       return <MaterialIcons name={name as any} className={className} />;
-    default:
+    case IconLibrary.FontAwesome:
       return <FontAwesome name={name as any} className={className} />;
+    case IconLibrary.CssClass:
+      return undefined;
+    default:
+      return (
+        // @ts-ignore
+        <FontAwesomeIcon icon={toIconClass() as any} className={className} />
+      );
+  }
+
+  function toIconClass() {
+    return iconLibrary + " fa-" + name;
   }
 }
