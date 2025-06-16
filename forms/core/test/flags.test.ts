ceaa47a5bf079979a56a8732beb9763ee8391590
Jolse Maginnis <doolse@gmail.com>
2025-06-16 22:39:29 +1000

    Some more

--- forms/core/test/flags.test.ts
+++ forms/core/test/flags.test.ts
@@ -1,8 +1,8 @@
 import { describe, it } from "@jest/globals";
 import fc from "fast-check";
-import { arbitraryControl } from "./gen";
+import { arbitraryControl, arbitraryData } from "./gen";
 import { testNodeState } from "./nodeTester";
-import { DynamicPropertyType } from "../src";
+import { DynamicPropertyType, isControlReadonly } from "../src";
 
 describe("hidden", () => {
   it("static hidden on all definitions", () => {
@@ -34,17 +34,59 @@ describe("hidden", () => {
             dynamic: [
               {
                 type: DynamicPropertyType.Visible,
-                expr: { type: "" },
+                expr: { type: "Anything" },
               },
             ],
           },
-          (_, ctx) => {
-            console.log("custom eval")
-              ctx.returnResult(!hidden);
-          },
+          (_, ctx) => ctx.returnResult(!hidden),
         );
-        console.log(hidden, firstChild.hidden);
         return !!(firstChild.hidden && firstChild.definition.hidden) === hidden;
+      }),
+    );
+  });
+
+  it("dynamic disabled on all definitions", () => {
+    fc.assert(
+      fc.property(arbitraryControl, fc.boolean(), (c, disabled) => {
+        const firstChild = testNodeState(
+          {
+            ...c,
+            dynamic: [
+              {
+                type: DynamicPropertyType.Disabled,
+                expr: { type: "Anything" },
+              },
+            ],
+          },
+          (_, ctx) => ctx.returnResult(disabled),
+        );
+        return (
+          !!(firstChild.disabled && firstChild.definition.disabled) === disabled
+        );
+      }),
+    );
+  });
+
+  it("dynamic readonly on all definitions", () => {
+    fc.assert(
+      fc.property(arbitraryData(), fc.boolean(), (c, readonly) => {
+        const firstChild = testNodeState(
+          {
+            ...c,
+            dynamic: [
+              {
+                type: DynamicPropertyType.Readonly,
+                expr: { type: "Anything" },
+              },
+            ],
+          },
+          (_, ctx) => ctx.returnResult(readonly),
+        );
+        return (
+          !!(
+            firstChild.readonly && isControlReadonly(firstChild.definition)
+          ) === readonly
+        );
       }),
     );
   });
