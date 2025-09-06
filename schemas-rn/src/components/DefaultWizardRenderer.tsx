aef9c911f4f5b5b092aff482998717c4d60c0fe7
Jolse Maginnis <doolse@gmail.com>
2025-09-06 11:02:47 +1000

    Missed these

---
+++ schemas-rn/src/components/DefaultWizardRenderer.tsx
@@ -0,0 +1,187 @@
+import {
+  ActionRendererProps,
+  createAction,
+  createGroupRenderer,
+  CustomNavigationProps,
+  deepMerge,
+  DefaultWizardRenderOptions,
+  fontAwesomeIcon,
+  FormRenderer,
+  GroupRendererProps,
+  GroupRenderType,
+  IconPlacement,
+  IconReference,
+  rendererClass,
+} from "@react-typed-forms/schemas";
+import { useComputed, useControl } from "@react-typed-forms/core";
+import { Fragment, ReactNode } from "react";
+
+
+const defaultOptions = {
+  classes: {
+    className: undefined,
+    contentClass: "min-h-96 overflow-auto",
+    navContainerClass: "flex justify-between gap-4 my-2",
+  },
+  actions: {
+    nextText: "Next",
+    nextIcon: fontAwesomeIcon("chevron-right"),
+    nextValidate: true,
+    prevText: "Prev",
+    prevIcon: fontAwesomeIcon("chevron-left"),
+    prevValidate: false,
+  },
+  renderNavigation: defaultNavigationRender,
+} satisfies DefaultWizardRenderOptions;
+
+function defaultNavigationRender({
+  formRenderer,
+  prev,
+  next,
+  className,
+}: CustomNavigationProps) {
+  {
+    const {
+      html: { Div },
+      renderAction,
+    } = formRenderer;
+    return (
+      <Div className={className}>
+        {renderAction(prev)}
+        {renderAction(next)}
+      </Div>
+    );
+  }
+}
+
+export function createWizardRenderer(options?: DefaultWizardRenderOptions) {
+  return createGroupRenderer(
+    (props, formRenderer) => (
+      <WizardRenderer
+        groupProps={props}
+        formRenderer={formRenderer}
+        options={options}
+      />
+    ),
+    {
+      renderType: GroupRenderType.Wizard,
+    },
+  );
+}
+
+function WizardRenderer({
+  groupProps: props,
+  formRenderer,
+  options,
+}: {
+  groupProps: GroupRendererProps;
+  formRenderer: FormRenderer;
+  options?: DefaultWizardRenderOptions;
+}) {
+  const mergedOptions = deepMerge(
+    (options ?? {}) as typeof defaultOptions,
+    defaultOptions,
+  );
+  const {
+    classes: { className, contentClass, navContainerClass },
+    actions: {
+      nextText,
+      nextIcon,
+      prevText,
+      prevIcon,
+      nextValidate,
+      prevValidate,
+    },
+    renderNavigation,
+  } = mergedOptions;
+  const { formNode, designMode, renderChild } = props;
+  const {
+    html: { Div },
+  } = formRenderer;
+  const childrenLength = formNode.getChildCount();
+  const page = useControl(0);
+  const currentPage = page.value;
+  const isValid = useComputed(() => isPageValid());
+
+  const next = createAction("nav", () => nav(1, nextValidate), nextText, {
+    hidden: !designMode && nextVisibleInDirection(1) == null,
+    disabled: !isValid.value,
+    icon: nextIcon,
+    iconPlacement: IconPlacement.AfterText,
+  });
+
+  const prev = createAction("nav", () => nav(-1, prevValidate), prevText, {
+    disabled: !designMode && nextVisibleInDirection(-1) == null,
+    icon: prevIcon,
+  });
+  const navElement = renderNavigation({
+    formRenderer,
+    page: countVisibleUntil(currentPage),
+    totalPages: countVisibleUntil(childrenLength),
+    prev,
+    next: next,
+    className: navContainerClass,
+    validatePage: async () => validatePage(),
+  });
+  const content = designMode ? (
+    <Div>{formNode.children.map((child) => renderChild(child))}</Div>
+  ) : currentPage < childrenLength ? (
+    <Div className={contentClass}>
+      {renderChild(formNode.getChild(currentPage)!)}
+    </Div>
+  ) : (
+    <Fragment />
+  );
+
+  return (
+    <Div className={rendererClass(props.className, className)}>
+      {content}
+      {navElement}
+    </Div>
+  );
+
+  function countVisibleUntil(untilPage: number) {
+    let count = 0;
+    for (let i = 0; i < untilPage && i < childrenLength; i++) {
+      if (formNode.getChild(i)!.visible) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  function nav(dir: number, validate: boolean) {
+    if (validate && !validatePage()) {
+      return;
+    }
+    const next = nextVisibleInDirection(dir);
+    if (next != null) {
+      page.value = next;
+    }
+  }
+
+  function nextVisibleInDirection(dir: number): number | null {
+    let next = currentPage + dir;
+    while (next >= 0 && next < childrenLength) {
+      if (formNode.getChild(next)!.visible) {
+        return next;
+      }
+      next += dir;
+    }
+    return null;
+  }
+
+  function validatePage() {
+    const pageNode = formNode.getChild(currentPage);
+    if (pageNode) {
+      const valid = pageNode.validate();
+      pageNode.setTouched(true);
+      return valid;
+    }
+    return false;
+  }
+
+  function isPageValid() {
+    return formNode.getChild(currentPage)!.valid;
+  }
+}
---
+++ schemas-rn/src/components/TabsRenderer.tsx
@@ -0,0 +1,110 @@
+import {
+  createGroupRenderer,
+  createScoped,
+  DefaultFormNodeUi,
+  DefaultTabsRenderOptions,
+  FormStateNode,
+  GroupRendererProps,
+  GroupRenderType,
+  rendererClass,
+  TabsRenderOptions,
+} from "@react-typed-forms/schemas";
+import React, { Fragment, useEffect } from "react";
+import clsx from "clsx";
+import { useControl } from "@react-typed-forms/core";
+import { Control } from "@astroapps/controls";
+
+export function createTabsRenderer(options: DefaultTabsRenderOptions = {}) {
+  return createGroupRenderer(
+    (p, renderer) => {
+      return (
+        <TabsRenderer
+          groupProps={p}
+          tabOptions={p.renderOptions as TabsRenderOptions}
+          options={options}
+        />
+      );
+    },
+    {
+      renderType: GroupRenderType.Tabs,
+    },
+  );
+}
+
+function TabsRenderer({
+  options,
+  groupProps: { designMode, formNode, className, renderChild },
+  tabOptions,
+}: {
+  options: DefaultTabsRenderOptions;
+  groupProps: GroupRendererProps;
+  tabOptions: TabsRenderOptions;
+}) {
+  const tabIndex = useControl(0);
+
+  useEffect(() => {
+    formNode.attachUi(new TabUi(tabIndex, formNode));
+  }, [formNode, tabIndex]);
+
+  const {
+    tabClass,
+    labelClass,
+    tabListClass,
+    inactiveClass,
+    activeClass,
+    contentClass,
+  } = options;
+  const currentTab = tabIndex.value;
+  return designMode ? (
+    <>{formNode.children.map((x, i) => renderTabs([x], i))}</>
+  ) : (
+    renderTabs(
+      formNode.children.filter((x, i) => x.visible),
+      0,
+    )
+  );
+
+  function renderTabs(tabs: FormStateNode[], key: number) {
+    return (
+      <div key={key} className={rendererClass(className, options.className)}>
+        <ul className={rendererClass(null, tabListClass)}>
+          {tabs.map((x, i) => (
+            <li
+              key={i}
+              className={rendererClass(null, tabClass)}
+              onClick={() => (tabIndex.value = i)}
+            >
+              <span
+                className={rendererClass(
+                  null,
+                  clsx(
+                    labelClass,
+                    i == currentTab ? activeClass : inactiveClass,
+                  ),
+                )}
+              >
+                {x.definition.title ? x.definition.title : "<untitled>"}
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div className={rendererClass(tabOptions.contentClass, contentClass)}>
+          {renderChild(tabs[currentTab])}
+        </div>
+      </div>
+    );
+  }
+}
+
+class TabUi extends DefaultFormNodeUi {
+  constructor(
+    private tabIndex: Control<number>,
+    node: FormStateNode,
+  ) {
+    super(node);
+  }
+  ensureChildVisible(childIndex: number) {
+    this.tabIndex.value = childIndex;
+    super.ensureChildVisible(childIndex);
+  }
+}
