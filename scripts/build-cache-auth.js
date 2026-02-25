9d59d011979acc5a3211ed1b73a6f07556601e27
Rhys <rhysg3@gmail.com>
2026-02-25 15:47:56 +1100

    add documentation for npmrc

--- scripts/build-cache-auth.js
+++ scripts/build-cache-auth.js
@@ -13,3 +13,9 @@ if (!match) {
 }
 
 console.log(`Bearer ${match[1].trim()}`);
+
+// Expected .npmrc format in ~/.npmrc:
+
+// @astroapps:registry=https://astro-registry.ngrok.app
+// @react-typed-forms:registry=https://astro-registry.ngrok.app
+// //astro-registry.ngrok.app/:_authToken=REA11y-5ECUR3-T0K3N-i5-5up3R-s3cr3t-4nd-sup3r-l0ng-t0k3n-1n-4-b34r3r-f0rm4t
