febf8dc9f2ef12813008939f3593985dd7f7f4da
Jolse Maginnis <doolse@gmail.com>
2026-04-01 14:12:43 +1100

    Support custom header content

--- astrolabe-schemas-datagrid/src/DataGridControlRenderer.tsx
+++ astrolabe-schemas-datagrid/src/DataGridControlRenderer.tsx
@@ -85,7 +85,14 @@ interface DataGridOptions
   disableClear?: boolean;
 }
 
-interface DataGridClasses {
+export interface DataGridHeaderContentProps {
+  column: ColumnDef<FormStateNode, DataGridColumnExtension>;
+  title: ReactNode;
+  filter: ReactNode;
+  sort: ReactNode;
+}
+
+export interface DataGridClasses {
   className?: string;
   popoverClass?: string;
   titleContainerClass?: string;
@@ -97,9 +104,10 @@ interface DataGridClasses {
   bodyCellClass?: string;
   clearFilterClass?: string;
   clearFilterText?: string;
+  renderHeaderContent?: (props: DataGridHeaderContentProps) => ReactNode;
 }
 
-interface DataGridColumnExtension {
+export interface DataGridColumnExtension {
   dataContext: ControlDataContext;
   definition: ControlDefinition;
 }
@@ -547,6 +555,14 @@ function DataGridControlRenderer({
           );
         }
       }
+    }
+    if (classes.renderHeaderContent) {
+      return classes.renderHeaderContent({
+        column: col,
+        title,
+        filter: filtered,
+        sort: sorted,
+      });
     }
     return (
       <div className={classes.titleContainerClass}>
